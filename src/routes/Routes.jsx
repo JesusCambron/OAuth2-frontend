2c8bf30ace796699121dec0d105ba162db777e23
JesusCambron <manuel.cam98@gmail.com>
2023-02-06 15:58:50 -0700

    Example

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, createRoutesFromElements, Route } from "react-rout
 import Login from "../pages/Login";
 import OauthPage from "../pages/OauthPage";
 import Signup from "../pages/Signup";
-
+/*  */
 export const Routes = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/oauth" element={<OauthPage />}>
